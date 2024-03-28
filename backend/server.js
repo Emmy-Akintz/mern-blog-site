868c73e9f1074bc7c6cccebbc2458c4cba9dc06b
Ayomide Akintan <emzyakints2005@gmail.com>
2024-03-28 14:01:35 +0100

    update server.js file and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-3.40
+const express = require('express')
+
+const app = express()
+
+//! PORT
+const PORT = 5000
+//! Start the server
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}`))
