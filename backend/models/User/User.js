f7cc9864923a1b9c5b015191a6e22fc4f2ea099b
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-09 15:54:15 +0100

    update

--- backend/models/User/User.js
+++ backend/models/User/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
-    // Basic user information
+    // Basic user information...
     username: {
       type: String,
       required: true,
