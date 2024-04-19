79168c37d020cbf4cef8b43d1fd88fe8b3f1e4b7
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-19 11:24:16 +0100

    working on post route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,24 @@
 const express = require('express')
-
+const Post = require('./models/Post/Post')
 const app = express()
+
+//* Middlewares
+app.use(express.json()) //parse json data
+
+//! Create post
+app.post('/api/v1/post/create', async (req, res) => {
+    try {
+        //* get the payload
+        const postData = req.body
+        Post.create()
+    } catch (error) {
+        
+    }
+})
+//! List post
+//! Update post
+//! Get post
+//! Delete post
 
 //! PORT
 const PORT = 5000
