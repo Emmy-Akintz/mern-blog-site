e00e5d9d220a124d5c730d49f00cd9d186b815be
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-19 18:05:51 +0100

    working on create post route

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,19 @@ const app = express()
 app.use(express.json()) //parse json data
 
 //! Create post
-app.post('/api/v1/post/create', async (req, res) => {
+app.post('/api/v1/posts/create', async (req, res) => {
     try {
         //* get the payload
         const postData = req.body
-        Post.create()
+        const postCreated = await Post.create(postData)
+        res.json({
+            'status': 'success',
+            'message': 'Post created successfully',
+            postCreated
+        })
     } catch (error) {
-        
+        console.log(error)
+        res.json(error)
     }
 })
 //! List post
