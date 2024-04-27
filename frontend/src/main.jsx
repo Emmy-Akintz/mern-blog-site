6c79aac364d4c9d63a580908f77ccb3f90e20f49
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-27 16:16:27 +0100

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 //! Create instance of a client
 const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
