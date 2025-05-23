777e36cfb1511703019424fa01cddb1c4ad96866
Ayomide Akintan <emzyakints2005@gmail.com>
2025-05-23 11:48:20 +0100

    route not found

--- backend/server.js
+++ backend/server.js
@@ -108,6 +108,11 @@ app.delete(
     })
 );
 
+//! Not found
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Route not found on our server" });
+});
+
 //! Error handling middleware
 app.use((err, req, res, next) => {
     // prepare the error messages
